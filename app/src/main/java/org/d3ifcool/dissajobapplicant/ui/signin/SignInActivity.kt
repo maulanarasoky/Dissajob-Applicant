7313ea8b64b54af02daa49b0c96e0a43daf6ae5c
maulanarasoky <rasoky.maulana@gmail.com>
2021-04-18 22:30:55 +0700

    Designing SignInActivity

---
+++ app/src/main/java/org/d3ifcool/dissajobapplicant/ui/signin/SignInActivity.kt
@@ -0,0 +1,12 @@
+package org.d3ifcool.dissajobapplicant.ui.signin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.d3ifcool.dissajobapplicant.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}

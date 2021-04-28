b451c27625af4bfd9edab78b265863495a409df3
maulanarasoky <rasoky.maulana@gmail.com>
2021-04-28 22:04:09 +0700

    Make settings account feature

---
+++ app/src/main/java/org/d3ifcool/dissajobapplicant/ui/settings/SettingsActivity.kt
@@ -0,0 +1,96 @@
+package org.d3ifcool.dissajobapplicant.ui.settings
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import org.d3ifcool.dissajobapplicant.R
+import org.d3ifcool.dissajobapplicant.databinding.ActivitySettingsBinding
+import org.d3ifcool.dissajobapplicant.ui.profile.ApplicantViewModel
+import org.d3ifcool.dissajobapplicant.ui.viewmodel.ViewModelFactory
+import org.d3ifcool.dissajobapplicant.utils.AuthHelper
+import org.d3ifcool.dissajobapplicant.vo.Status
+
+class SettingsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var activitySettingsBinding: ActivitySettingsBinding
+
+    private lateinit var viewModel: ApplicantViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activitySettingsBinding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(activitySettingsBinding.root)
+
+        activitySettingsBinding.toolbar.title = resources.getString(R.string.txt_settings)
+        setSupportActionBar(activitySettingsBinding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        val factory = ViewModelFactory.getInstance(this)
+        viewModel = ViewModelProvider(this, factory)[ApplicantViewModel::class.java]
+        showCurrentProfileData()
+
+        //Change profile section
+        activitySettingsBinding.profileSection.btnChangeProfile.setOnClickListener(this)
+        activitySettingsBinding.profileSection.btnChangePhoneNumber.setOnClickListener(this)
+        activitySettingsBinding.profileSection.btnChangeEmail.setOnClickListener(this)
+
+        //Change password section
+        activitySettingsBinding.securitySection.btnChangePassword.setOnClickListener(this)
+    }
+
+    private fun showCurrentProfileData() {
+        viewModel.getApplicantDetails(AuthHelper.currentUser?.uid.toString())
+            .observe(this) { profileData ->
+                if (profileData.data != null) {
+                    when (profileData.status) {
+                        Status.LOADING -> {
+                        }
+                        Status.SUCCESS -> {
+                            activitySettingsBinding.profileSection.tvPhoneNumber.text = profileData.data.phoneNumber.toString()
+                            activitySettingsBinding.profileSection.tvEmail.text = profileData.data.email.toString()
+                        }
+                        Status.ERROR -> {
+                            Toast.makeText(this, "Error occurred", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btnChangeProfile -> {
+                startActivity(Intent(this, ChangeProfileActivity::class.java))
+            }
+            R.id.btnChangePhoneNumber -> {
+                startActivity(Intent(this, ChangePhoneNumberActivity::class.java))
+            }
+            R.id.btnChangeEmail -> {
+                startActivity(Intent(this, ChangeEmailActivity::class.java))
+            }
+            R.id.btnChangePassword -> {
+                startActivity(Intent(this, ChangePasswordActivity::class.java))
+            }
+        }
+    }
+}

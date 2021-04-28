15667492ecbf159f89ff654e77086bd068c5fb4a
maulanarasoky <rasoky.maulana@gmail.com>
2021-04-29 01:30:05 +0700

    Finish implementing getApplications function on ApplicationActivity

---
+++ app/src/main/java/org/d3ifcool/dissajobapplicant/ui/application/ApplicationActivity.kt
@@ -0,0 +1,117 @@
+package org.d3ifcool.dissajobapplicant.ui.application
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import org.d3ifcool.dissajobapplicant.data.source.local.entity.job.JobEntity
+import org.d3ifcool.dissajobapplicant.data.source.local.entity.recruiter.RecruiterEntity
+import org.d3ifcool.dissajobapplicant.databinding.ActivityApplicationBinding
+import org.d3ifcool.dissajobapplicant.ui.job.JobDetailsActivity
+import org.d3ifcool.dissajobapplicant.ui.job.JobViewModel
+import org.d3ifcool.dissajobapplicant.ui.job.callback.ItemClickListener
+import org.d3ifcool.dissajobapplicant.ui.job.callback.LoadJobByIdCallback
+import org.d3ifcool.dissajobapplicant.ui.recruiter.LoadRecruiterDataCallback
+import org.d3ifcool.dissajobapplicant.ui.recruiter.RecruiterViewModel
+import org.d3ifcool.dissajobapplicant.ui.viewmodel.ViewModelFactory
+import org.d3ifcool.dissajobapplicant.vo.Status
+
+class ApplicationActivity : AppCompatActivity(), ItemClickListener, LoadJobByIdCallback,
+    LoadRecruiterDataCallback {
+
+    private lateinit var activityApplicationBinding: ActivityApplicationBinding
+
+    private lateinit var applicationAdapter: ApplicationAdapter
+
+    private lateinit var applicationViewModel: ApplicationViewModel
+
+    private lateinit var jobViewModel: JobViewModel
+
+    private lateinit var recruiterViewModel: RecruiterViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityApplicationBinding = ActivityApplicationBinding.inflate(layoutInflater)
+        setContentView(activityApplicationBinding.root)
+
+        val factory = ViewModelFactory.getInstance(this)
+
+        applicationViewModel = ViewModelProvider(this, factory)[ApplicationViewModel::class.java]
+        jobViewModel = ViewModelProvider(this, factory)[JobViewModel::class.java]
+        recruiterViewModel = ViewModelProvider(this, factory)[RecruiterViewModel::class.java]
+
+        applicationAdapter = ApplicationAdapter(this, this, this)
+
+        applicationViewModel.getApplications().observe(this) { jobs ->
+            if (jobs.data != null) {
+                when (jobs.status) {
+                    Status.LOADING -> showLoading(true)
+                    Status.SUCCESS -> {
+                        showLoading(false)
+                        if (jobs.data.isNotEmpty()) {
+                            applicationAdapter.submitList(jobs.data)
+                            applicationAdapter.notifyDataSetChanged()
+                        } else {
+                            activityApplicationBinding.tvNoData.visibility = View.VISIBLE
+                        }
+                    }
+                    Status.ERROR -> {
+                        showLoading(false)
+                        Toast.makeText(this, "Error occurred", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+        with(activityApplicationBinding.rvJob) {
+            layoutManager = LinearLayoutManager(this@ApplicationActivity)
+            setHasFixedSize(true)
+            addItemDecoration(
+                DividerItemDecoration(
+                    this@ApplicationActivity,
+                    DividerItemDecoration.VERTICAL
+                )
+            )
+            adapter = applicationAdapter
+        }
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            activityApplicationBinding.progressBar.visibility = View.VISIBLE
+        } else {
+            activityApplicationBinding.progressBar.visibility = View.GONE
+        }
+    }
+
+    override fun onItemClicked(jobId: String) {
+        val intent = Intent(this, JobDetailsActivity::class.java)
+        intent.putExtra(JobDetailsActivity.EXTRA_ID, jobId)
+        startActivity(intent)
+    }
+
+    override fun onLoadJobData(jobId: String, callback: LoadJobByIdCallback) {
+        jobViewModel.getJobById(jobId).observe(this) { job ->
+            if (job.data != null) {
+                callback.onJobReceived(job.data)
+            }
+        }
+    }
+
+    override fun onJobReceived(jobEntity: JobEntity) {
+    }
+
+    override fun onLoadRecruiterData(recruiterId: String, callback: LoadRecruiterDataCallback) {
+        recruiterViewModel.getRecruiterData(recruiterId).observe(this) { recruiterDetails ->
+            if (recruiterDetails != null) {
+                recruiterDetails.data?.let { callback.onRecruiterDataReceived(it) }
+            }
+        }
+    }
+
+    override fun onRecruiterDataReceived(recruiterData: RecruiterEntity) {
+    }
+}
---
+++ app/src/main/java/org/d3ifcool/dissajobapplicant/ui/application/ApplicationAdapter.kt
@@ -0,0 +1,133 @@
+package org.d3ifcool.dissajobapplicant.ui.application
+
+import android.text.format.DateUtils
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import org.d3ifcool.dissajobapplicant.R
+import org.d3ifcool.dissajobapplicant.data.source.local.entity.application.ApplicationEntity
+import org.d3ifcool.dissajobapplicant.data.source.local.entity.job.JobEntity
+import org.d3ifcool.dissajobapplicant.data.source.local.entity.recruiter.RecruiterEntity
+import org.d3ifcool.dissajobapplicant.databinding.JobItemBinding
+import org.d3ifcool.dissajobapplicant.ui.job.callback.ItemClickListener
+import org.d3ifcool.dissajobapplicant.ui.job.callback.LoadJobByIdCallback
+import org.d3ifcool.dissajobapplicant.ui.recruiter.LoadRecruiterDataCallback
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+class ApplicationAdapter(
+    private val clickCallback: ItemClickListener,
+    private val loadJobDataCallback: LoadJobByIdCallback,
+    private val loadRecruiterDataCallback: LoadRecruiterDataCallback
+) : PagedListAdapter<ApplicationEntity, ApplicationAdapter.JobViewHolder>(DIFF_CALLBACK) {
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<ApplicationEntity>() {
+            override fun areItemsTheSame(
+                oldItem: ApplicationEntity,
+                newItem: ApplicationEntity
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(
+                oldItem: ApplicationEntity,
+                newItem: ApplicationEntity
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JobViewHolder {
+        val itemsJobBinding = JobItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return JobViewHolder(itemsJobBinding)
+    }
+
+    override fun onBindViewHolder(holder: JobViewHolder, position: Int) {
+        val job = getItem(position)
+        if (job != null) {
+            holder.bindItem(job)
+        }
+    }
+
+    inner class JobViewHolder(private val binding: JobItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bindItem(items: ApplicationEntity) {
+            loadJobData(items.jobId.toString())
+        }
+
+        private fun loadJobData(jobId: String) {
+            loadJobDataCallback.onLoadJobData(jobId, object : LoadJobByIdCallback {
+                override fun onLoadJobData(jobId: String, callback: LoadJobByIdCallback) {
+                }
+
+                override fun onJobReceived(jobEntity: JobEntity) {
+                    with(binding) {
+                        tvJobTitle.text = jobEntity.title.toString()
+                        tvJobAddress.text = jobEntity.address.toString()
+                        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+                        sdf.timeZone = TimeZone.getTimeZone("Asia/Jakarta")
+                        try {
+                            val time: Long = sdf.parse(jobEntity.postedDate).time
+                            val now = System.currentTimeMillis()
+                            val ago =
+                                DateUtils.getRelativeTimeSpanString(time, now, DateUtils.MINUTE_IN_MILLIS)
+                            tvJobPostedDate.text = ago
+                        } catch (e: ParseException) {
+                            e.printStackTrace()
+                        }
+
+                        loadRecruiterData(jobEntity.postedBy.toString())
+
+                        itemView.setOnClickListener {
+                            clickCallback.onItemClicked(jobId)
+                        }
+                    }
+                }
+
+            })
+        }
+
+        private fun loadRecruiterData(recruiterId: String) {
+            loadRecruiterDataCallback.onLoadRecruiterData(recruiterId, object :
+                LoadRecruiterDataCallback {
+                override fun onLoadRecruiterData(
+                    recruiterId: String,
+                    callback: LoadRecruiterDataCallback
+                ) {
+                }
+
+                override fun onRecruiterDataReceived(recruiterData: RecruiterEntity) {
+                    with(binding) {
+                        tvJobRecruiterName.text = recruiterData.fullName.toString()
+                        if (recruiterData.imagePath != "-") {
+                            val storageRef = Firebase.storage.reference
+                            val circularProgressDrawable = CircularProgressDrawable(itemView.context)
+                            circularProgressDrawable.strokeWidth = 5f
+                            circularProgressDrawable.centerRadius = 30f
+                            circularProgressDrawable.start()
+                            Glide.with(itemView.context)
+                                .load(storageRef.child("recruiter/profile/images/${recruiterData.imagePath}"))
+                                .transform(RoundedCorners(20))
+                                .apply(RequestOptions.placeholderOf(circularProgressDrawable))
+                                .error(R.drawable.ic_image_gray_24dp)
+                                .into(imgRecruiterPicture)
+                        }
+                    }
+                }
+
+            })
+        }
+    }
+}

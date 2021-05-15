2e2749887b258b5c444cdb536cc43194b653d2f6
maulanarasoky <rasoky.maulana@gmail.com>
2021-05-15 21:30:27 +0700

    Design application details activity

---
+++ app/src/main/java/org/d3ifcool/dissajobapplicant/ui/application/ApplicationDetailsActivity.kt
@@ -0,0 +1,15 @@
+package org.d3ifcool.dissajobapplicant.ui.application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.d3ifcool.dissajobapplicant.R
+
+class ApplicationDetailsActivity : AppCompatActivity() {
+
+    private lateinit var 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_application_details)
+    }
+}

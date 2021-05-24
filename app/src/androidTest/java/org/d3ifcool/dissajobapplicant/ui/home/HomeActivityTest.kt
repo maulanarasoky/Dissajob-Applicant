660a199a3d44cc046df0f4105374584f5577978d
maulanarasoky <rasoky.maulana@gmail.com>
2021-05-24 07:47:20 +0700

    Make instrumentation test to load job details

--- app/src/androidTest/java/org/d3ifcool/dissajobapplicant/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/org/d3ifcool/dissajobapplicant/ui/home/HomeActivityTest.kt
@@ -41,4 +41,43 @@ class HomeActivityTest {
             )
         )
     }
+
+    @Test
+    fun loadJobDetails() {
+        onView(withId(R.id.rvJob)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                1,
+                click()
+            )
+        )
+
+        onView(withId(R.id.tvJobTitle)).check(matches(isDisplayed()))
+        onView(withId(R.id.tvJobTitle)).check(matches(withText(dummyDetailsJob.title)))
+
+        onView(withId(R.id.tvJobRecruiterName)).check(matches(isDisplayed()))
+        onView(withId(R.id.tvJobRecruiterName)).check(matches(withText(dummyRecruiterData.fullName)))
+
+        onView(withId(R.id.tvJobAddress)).check(matches(isDisplayed()))
+        onView(withId(R.id.tvJobAddress)).check(matches(withText(dummyDetailsJob.address)))
+
+        onView(withId(R.id.tvJobType)).check(matches(isDisplayed()))
+        onView(withId(R.id.tvJobType)).check(matches(withText("${dummyDetailsJob.employment} \u2022 ${dummyDetailsJob.type}")))
+
+        onView(withId(R.id.imgRecruiterPicture)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.tvJobPostedDate)).check(matches(isDisplayed()))
+        onView(withId(R.id.tvJobPostedDate)).check(matches(withText(dummyDetailsJob.postedDate)))
+
+        onView(withId(R.id.tvJobDescription)).check(matches(isDisplayed()))
+        onView(withId(R.id.tvJobDescription)).check(matches(withText(dummyDetailsJob.description)))
+
+        onView(withId(R.id.tvJobQualification)).check(matches(isDisplayed()))
+        onView(withId(R.id.tvJobQualification)).check(matches(withText(dummyDetailsJob.qualification)))
+
+        onView(withId(R.id.tvJobIndustry)).check(matches(isDisplayed()))
+        onView(withId(R.id.tvJobIndustry)).check(matches(withText(dummyDetailsJob.industry)))
+
+        onView(withId(R.id.tvJobSalary)).check(matches(isDisplayed()))
+        onView(withId(R.id.tvJobSalary)).check(matches(withText(dummyDetailsJob.salary)))
+    }
 }

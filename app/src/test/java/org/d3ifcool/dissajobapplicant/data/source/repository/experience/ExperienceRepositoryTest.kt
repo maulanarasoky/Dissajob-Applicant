7b1949be5325a7bed22b685fe70a4ade1cd996e7
maulanarasoky <rasoky.maulana@gmail.com>
2021-06-19 09:47:57 +0700

    Make experience repository unit test

---
+++ app/src/test/java/org/d3ifcool/dissajobapplicant/data/source/repository/experience/ExperienceRepositoryTest.kt
@@ -0,0 +1,44 @@
+package org.d3ifcool.dissajobapplicant.data.source.repository.experience
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.paging.DataSource
+import org.d3ifcool.dissajobapplicant.data.source.local.entity.experience.ExperienceEntity
+import org.d3ifcool.dissajobapplicant.data.source.local.source.LocalExperienceSource
+import org.d3ifcool.dissajobapplicant.data.source.remote.source.RemoteExperienceSource
+import org.d3ifcool.dissajobapplicant.utils.AppExecutors
+import org.d3ifcool.dissajobapplicant.utils.PagedListUtil
+import org.d3ifcool.dissajobapplicant.utils.dummy.ApplicantDummy
+import org.d3ifcool.dissajobapplicant.utils.dummy.ExperienceDummy
+import org.d3ifcool.dissajobapplicant.vo.Resource
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.*
+
+class ExperienceRepositoryTest {
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val remote = mock(RemoteExperienceSource::class.java)
+    private val local = mock(LocalExperienceSource::class.java)
+    private val appExecutors = mock(AppExecutors::class.java)
+
+    private val experienceRepository = FakeExperienceRepository(remote, local, appExecutors)
+    private val experienceResponse = ExperienceDummy.generateExperienceData()
+    private val applicantData = ApplicantDummy.generateApplicantData()
+
+    @Test
+    fun getExperiencesTest() {
+        val dataSourceFactory =
+            mock(DataSource.Factory::class.java) as DataSource.Factory<Int, ExperienceEntity>
+        `when`(local.getApplicantExperiences(applicantData.id)).thenReturn(dataSourceFactory)
+        experienceRepository.getApplicantExperiences(applicantData.id)
+
+        val experienceEntities =
+            Resource.success(PagedListUtil.mockPagedList(ExperienceDummy.generateExperienceData()))
+        verify(local).getApplicantExperiences(applicantData.id)
+        assertNotNull(experienceEntities.data)
+        assertEquals(experienceResponse.size.toLong(), experienceEntities.data?.size?.toLong())
+    }
+}
---
+++ app/src/test/java/org/d3ifcool/dissajobapplicant/data/source/repository/experience/FakeExperienceRepository.kt
@@ -0,0 +1,101 @@
+package org.d3ifcool.dissajobapplicant.data.source.repository.experience
+
+import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import org.d3ifcool.dissajobapplicant.data.NetworkBoundResource
+import org.d3ifcool.dissajobapplicant.data.source.local.entity.experience.ExperienceEntity
+import org.d3ifcool.dissajobapplicant.data.source.local.source.LocalExperienceSource
+import org.d3ifcool.dissajobapplicant.data.source.remote.ApiResponse
+import org.d3ifcool.dissajobapplicant.data.source.remote.response.entity.experience.ExperienceResponseEntity
+import org.d3ifcool.dissajobapplicant.data.source.remote.source.RemoteExperienceSource
+import org.d3ifcool.dissajobapplicant.ui.experience.callback.AddExperienceCallback
+import org.d3ifcool.dissajobapplicant.ui.experience.callback.DeleteExperienceCallback
+import org.d3ifcool.dissajobapplicant.ui.experience.callback.LoadExperiencesCallback
+import org.d3ifcool.dissajobapplicant.ui.experience.callback.UpdateExperienceCallback
+import org.d3ifcool.dissajobapplicant.utils.AppExecutors
+import org.d3ifcool.dissajobapplicant.vo.Resource
+
+class FakeExperienceRepository(
+    private val remoteExperienceSource: RemoteExperienceSource,
+    private val localExperienceSource: LocalExperienceSource,
+    private val appExecutors: AppExecutors
+) :
+    ExperienceDataSource {
+
+    override fun getApplicantExperiences(applicantId: String): LiveData<Resource<PagedList<ExperienceEntity>>> {
+        return object :
+            NetworkBoundResource<PagedList<ExperienceEntity>, List<ExperienceResponseEntity>>(
+                appExecutors
+            ) {
+            public override fun loadFromDB(): LiveData<PagedList<ExperienceEntity>> {
+                val config = PagedList.Config.Builder()
+                    .setEnablePlaceholders(false)
+                    .setInitialLoadSizeHint(4)
+                    .setPageSize(4)
+                    .build()
+                return LivePagedListBuilder(
+                    localExperienceSource.getApplicantExperiences(
+                        applicantId
+                    ), config
+                ).build()
+            }
+
+            override fun shouldFetch(data: PagedList<ExperienceEntity>?): Boolean =
+                data == null
+
+            public override fun createCall(): LiveData<ApiResponse<List<ExperienceResponseEntity>>> =
+                remoteExperienceSource.getApplicantExperiences(
+                    applicantId,
+                    object : LoadExperiencesCallback {
+                        override fun onAllExperiencesReceived(experienceResponse: List<ExperienceResponseEntity>): List<ExperienceResponseEntity> {
+                            return experienceResponse
+                        }
+                    })
+
+            public override fun saveCallResult(data: List<ExperienceResponseEntity>) {
+                localExperienceSource.deleteAllApplicantExperiences(applicantId)
+                val experienceList = ArrayList<ExperienceEntity>()
+                for (response in data) {
+                    val experience = ExperienceEntity(
+                        response.id,
+                        response.title,
+                        response.employmentType,
+                        response.companyName,
+                        response.location,
+                        response.startMonth,
+                        response.startYear,
+                        response.endMonth,
+                        response.endYear,
+                        response.description,
+                        response.isCurrentlyWorking,
+                        response.applicantId
+                    )
+                    experienceList.add(experience)
+                }
+                localExperienceSource.insertApplicantExperiences(experienceList)
+            }
+        }.asLiveData()
+    }
+
+    override fun addApplicantExperience(
+        experience: ExperienceResponseEntity,
+        callback: AddExperienceCallback
+    ) =
+        appExecutors.diskIO()
+            .execute { remoteExperienceSource.addApplicantExperience(experience, callback) }
+
+    override fun updateApplicantExperience(
+        experience: ExperienceResponseEntity,
+        callback: UpdateExperienceCallback
+    ) =
+        appExecutors.diskIO()
+            .execute { remoteExperienceSource.updateApplicantExperience(experience, callback) }
+
+    override fun deleteApplicantExperience(id: String, callback: DeleteExperienceCallback) =
+        appExecutors.diskIO()
+            .execute {
+                localExperienceSource.deleteExperienceById(id)
+                remoteExperienceSource.deleteApplicantExperience(id, callback)
+            }
+}

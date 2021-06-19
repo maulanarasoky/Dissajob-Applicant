f29b34c8ae19351496cc4ab76ae52efd5d257626
maulanarasoky <rasoky.maulana@gmail.com>
2021-06-19 09:40:47 +0700

    Make application and education repository unit test

--- app/src/test/java/org/d3ifcool/dissajobapplicant/data/source/repository/application/ApplicationRepositoryTest.kt
+++ app/src/test/java/org/d3ifcool/dissajobapplicant/data/source/repository/application/ApplicationRepositoryTest.kt
@@ -1,14 +1,23 @@
 package org.d3ifcool.dissajobapplicant.data.source.repository.application
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import junit.framework.TestCase
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.DataSource
+import org.d3ifcool.dissajobapplicant.data.source.local.entity.application.ApplicationEntity
 import org.d3ifcool.dissajobapplicant.data.source.local.source.LocalApplicationSource
 import org.d3ifcool.dissajobapplicant.data.source.remote.source.RemoteApplicationSource
 import org.d3ifcool.dissajobapplicant.utils.AppExecutors
+import org.d3ifcool.dissajobapplicant.utils.PagedListUtil
+import org.d3ifcool.dissajobapplicant.utils.dummy.ApplicantDummy
 import org.d3ifcool.dissajobapplicant.utils.dummy.ApplicationDummy
+import org.d3ifcool.dissajobapplicant.utils.dummy.JobDummy
 import org.d3ifcool.dissajobapplicant.utils.dummy.RecruiterDummy
+import org.d3ifcool.dissajobapplicant.vo.Resource
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Rule
-import org.mockito.Mockito.mock
+import org.junit.Test
+import org.mockito.Mockito.*
 
 class ApplicationRepositoryTest {
     @get:Rule
@@ -24,5 +33,131 @@ class ApplicationRepositoryTest {
     private val acceptedApplicationResponse = ApplicationDummy.generateAcceptedApplicationsData()
     private val rejectedApplicationResponse = ApplicationDummy.generateRejectedApplicationsData()
     private val markedApplicationResponse = ApplicationDummy.generateMarkedApplicationsData()
+
     private val recruiterData = RecruiterDummy.generateRecruiterDetails()
+    private val jobData = JobDummy.generateJobsData()
+    private val applicantData = ApplicantDummy.generateApplicantData()
+
+    @Test
+    fun getApplicationsTest() {
+        val dataSourceFactory =
+            mock(DataSource.Factory::class.java) as DataSource.Factory<Int, ApplicationEntity>
+        `when`(local.getApplications(recruiterData.id)).thenReturn(dataSourceFactory)
+        applicationRepository.getApplications(recruiterData.id)
+
+        val applicationEntities =
+            Resource.success(PagedListUtil.mockPagedList(ApplicationDummy.generateApplicationsData()))
+        verify(local).getApplications(recruiterData.id)
+        assertNotNull(applicationEntities.data)
+        assertEquals(applicationResponse.size.toLong(), applicationEntities.data?.size?.toLong())
+    }
+
+    @Test
+    fun getAcceptedApplicationsTest() {
+        val dataSourceFactory =
+            mock(DataSource.Factory::class.java) as DataSource.Factory<Int, ApplicationEntity>
+        `when`(local.getAcceptedApplications(recruiterData.id)).thenReturn(dataSourceFactory)
+        applicationRepository.getAcceptedApplications(recruiterData.id)
+
+        val applicationEntities =
+            Resource.success(PagedListUtil.mockPagedList(ApplicationDummy.generateApplicationsData()))
+        verify(local).getAcceptedApplications(recruiterData.id)
+        assertNotNull(applicationEntities.data)
+        assertEquals(
+            acceptedApplicationResponse.size.toLong(),
+            applicationEntities.data?.size?.toLong()
+        )
+    }
+
+    @Test
+    fun getRejectedApplicationsTest() {
+        val dataSourceFactory =
+            mock(DataSource.Factory::class.java) as DataSource.Factory<Int, ApplicationEntity>
+        `when`(local.getRejectedApplications(recruiterData.id)).thenReturn(dataSourceFactory)
+        applicationRepository.getRejectedApplications(recruiterData.id)
+
+        val applicationEntities =
+            Resource.success(PagedListUtil.mockPagedList(ApplicationDummy.generateApplicationsData()))
+        verify(local).getRejectedApplications(recruiterData.id)
+        assertNotNull(applicationEntities.data)
+        assertEquals(
+            rejectedApplicationResponse.size.toLong(),
+            applicationEntities.data?.size?.toLong()
+        )
+    }
+
+    @Test
+    fun getMarkedApplicationsTest() {
+        val dataSourceFactory =
+            mock(DataSource.Factory::class.java) as DataSource.Factory<Int, ApplicationEntity>
+        `when`(local.getMarkedApplications(recruiterData.id)).thenReturn(dataSourceFactory)
+        applicationRepository.getMarkedApplications(recruiterData.id)
+
+        val applicationEntities =
+            Resource.success(PagedListUtil.mockPagedList(ApplicationDummy.generateApplicationsData()))
+        verify(local).getMarkedApplications(recruiterData.id)
+        assertNotNull(applicationEntities.data)
+        assertEquals(
+            markedApplicationResponse.size.toLong(),
+            applicationEntities.data?.size?.toLong()
+        )
+    }
+
+    @Test
+    fun getApplicationByJob() {
+        val applicationLiveData = MutableLiveData<ApplicationEntity>()
+        `when`(local.getApplicationByJob(jobData[0].id, applicantData.id)).thenReturn(
+            applicationLiveData
+        )
+        applicationRepository.getApplicationByJob(jobData[0].id, applicantData.id)
+
+        val applicationEntity = Resource.success(ApplicationDummy.generateApplicationsData())
+        verify(local).getApplicationByJob(jobData[0].id, applicantData.id)
+        assertNotNull(applicationEntity.data)
+        assertEquals(applicationResponse[0].id, applicationEntity.data?.get(0)?.id)
+        assertEquals(
+            applicationResponse[0].applicantId,
+            applicationEntity.data?.get(0)?.applicantId
+        )
+        assertEquals(applicationResponse[0].jobId, applicationEntity.data?.get(0)?.jobId)
+        assertEquals(applicationResponse[0].applyDate, applicationEntity.data?.get(0)?.applyDate)
+        assertEquals(
+            applicationResponse[0].updatedDate,
+            applicationEntity.data?.get(0)?.updatedDate
+        )
+        assertEquals(applicationResponse[0].status, applicationEntity.data?.get(0)?.status)
+        assertEquals(applicationResponse[0].isMarked, applicationEntity.data?.get(0)?.isMarked)
+        assertEquals(
+            applicationResponse[0].recruiterId,
+            applicationEntity.data?.get(0)?.recruiterId
+        )
+    }
+
+    @Test
+    fun getApplicationByIdTest() {
+        val applicationLiveData = MutableLiveData<ApplicationEntity>()
+        `when`(local.getApplicationById(applicationResponse[0].id)).thenReturn(applicationLiveData)
+        applicationRepository.getApplicationById(applicationResponse[0].id)
+
+        val applicationEntity = Resource.success(ApplicationDummy.generateApplicationsData())
+        verify(local).getApplicationById(applicationResponse[0].id)
+        assertNotNull(applicationEntity.data)
+        assertEquals(applicationResponse[0].id, applicationEntity.data?.get(0)?.id)
+        assertEquals(
+            applicationResponse[0].applicantId,
+            applicationEntity.data?.get(0)?.applicantId
+        )
+        assertEquals(applicationResponse[0].jobId, applicationEntity.data?.get(0)?.jobId)
+        assertEquals(applicationResponse[0].applyDate, applicationEntity.data?.get(0)?.applyDate)
+        assertEquals(
+            applicationResponse[0].updatedDate,
+            applicationEntity.data?.get(0)?.updatedDate
+        )
+        assertEquals(applicationResponse[0].status, applicationEntity.data?.get(0)?.status)
+        assertEquals(applicationResponse[0].isMarked, applicationEntity.data?.get(0)?.isMarked)
+        assertEquals(
+            applicationResponse[0].recruiterId,
+            applicationEntity.data?.get(0)?.recruiterId
+        )
+    }
 }
---
+++ app/src/test/java/org/d3ifcool/dissajobapplicant/data/source/repository/education/FakeEducationRepository.kt
@@ -0,0 +1,102 @@
+package org.d3ifcool.dissajobapplicant.data.source.repository.education
+
+import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import org.d3ifcool.dissajobapplicant.data.NetworkBoundResource
+import org.d3ifcool.dissajobapplicant.data.source.local.entity.education.EducationEntity
+import org.d3ifcool.dissajobapplicant.data.source.local.source.LocalEducationSource
+import org.d3ifcool.dissajobapplicant.data.source.remote.ApiResponse
+import org.d3ifcool.dissajobapplicant.data.source.remote.response.entity.education.EducationResponseEntity
+import org.d3ifcool.dissajobapplicant.data.source.remote.source.RemoteEducationSource
+import org.d3ifcool.dissajobapplicant.ui.education.callback.AddEducationCallback
+import org.d3ifcool.dissajobapplicant.ui.education.callback.DeleteEducationCallback
+import org.d3ifcool.dissajobapplicant.ui.education.callback.LoadEducationsCallback
+import org.d3ifcool.dissajobapplicant.ui.education.callback.UpdateEducationCallback
+import org.d3ifcool.dissajobapplicant.utils.AppExecutors
+import org.d3ifcool.dissajobapplicant.vo.Resource
+
+class FakeEducationRepository(
+    private val remoteEducationSource: RemoteEducationSource,
+    private val localEducationSource: LocalEducationSource,
+    private val appExecutors: AppExecutors
+) :
+    EducationDataSource {
+
+    override fun getApplicantEducations(applicantId: String): LiveData<Resource<PagedList<EducationEntity>>> {
+        return object :
+            NetworkBoundResource<PagedList<EducationEntity>, List<EducationResponseEntity>>(
+                appExecutors
+            ) {
+            public override fun loadFromDB(): LiveData<PagedList<EducationEntity>> {
+                val config = PagedList.Config.Builder()
+                    .setEnablePlaceholders(false)
+                    .setInitialLoadSizeHint(4)
+                    .setPageSize(4)
+                    .build()
+                return LivePagedListBuilder(
+                    localEducationSource.getApplicantEducations(applicantId),
+                    config
+                ).build()
+            }
+
+            override fun shouldFetch(data: PagedList<EducationEntity>?): Boolean =
+                data == null
+
+            public override fun createCall(): LiveData<ApiResponse<List<EducationResponseEntity>>> =
+                remoteEducationSource.getApplicantEducations(
+                    applicantId,
+                    object : LoadEducationsCallback {
+                        override fun onAllEducationsReceived(educationResponse: List<EducationResponseEntity>): List<EducationResponseEntity> {
+                            return educationResponse
+                        }
+                    })
+
+            public override fun saveCallResult(data: List<EducationResponseEntity>) {
+                localEducationSource.deleteAllApplicantEducations(applicantId)
+                val educationList = ArrayList<EducationEntity>()
+                for (response in data) {
+                    val education = EducationEntity(
+                        response.id,
+                        response.schoolName,
+                        response.educationLevel,
+                        response.fieldOfStudy,
+                        response.startMonth,
+                        response.startYear,
+                        response.endMonth,
+                        response.endYear,
+                        response.description,
+                        response.applicantId
+                    )
+                    educationList.add(education)
+                }
+                localEducationSource.insertApplicantEducations(educationList)
+            }
+        }.asLiveData()
+    }
+
+    override fun addApplicantEducation(
+        education: EducationResponseEntity,
+        callback: AddEducationCallback
+    ) =
+        appExecutors.diskIO()
+            .execute { remoteEducationSource.addApplicantEducation(education, callback) }
+
+    override fun updateApplicantEducation(
+        education: EducationResponseEntity,
+        callback: UpdateEducationCallback
+    ) = appExecutors.diskIO()
+        .execute {
+            remoteEducationSource.updateApplicantEducation(
+                education,
+                callback
+            )
+        }
+
+    override fun deleteApplicantEducation(id: String, callback: DeleteEducationCallback) =
+        appExecutors.diskIO()
+            .execute {
+                localEducationSource.deleteEducationById(id)
+                remoteEducationSource.deleteApplicantEducation(id, callback)
+            }
+}
